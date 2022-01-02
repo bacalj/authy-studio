4c56444184c4c642d41a93bb6a041d014b69c60a
Joseph Bacal <bacalj@gmail.com>
2022-01-01 21:03:44 -0500

    initial commit

---
+++ schemas/schema.js
@@ -0,0 +1,13 @@
+import createSchema from 'part:@sanity/base/schema-creator'
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+import note from './note'
+import webUser from './webUser'
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    webUser,
+    note
+  ]),
+})
---
+++ schemas/webUser.js
@@ -0,0 +1,24 @@
+export default {
+  name: 'webUser',
+  title: 'Web User',
+  type: 'document',
+  fields: [
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string'
+    },
+    {
+      name: 'Notes',
+      title: 'notes',
+      type: 'array',
+      of: [
+        { type: 'reference',
+          to: [ 
+            { type: 'note' }
+          ]
+        }
+      ]
+    }
+  ]
+}
